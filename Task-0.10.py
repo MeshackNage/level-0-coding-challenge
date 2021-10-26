2fbd88b100a80ef1c57076d5cda44cfd317677e6
Meshack Nage <meshack.nage@gmail.com>
2021-10-26 10:15:08 +0200

    Updated task 0.10

--- Task-0.10.py
+++ Task-0.10.py
@@ -1,12 +1,10 @@
-def common_characters(string_1, string_2):
+def find_common_characters(string_1, string_2):
     common = []
-    i = ""
     for x in string_1.lower():
         if x in string_2.lower():
             if x not in common:
                 common.append(x)
     print("Common characters:", ', '.join(common))
-    return i
 
 
-common_characters("Eckard", "Berry")
+find_common_characters("Eckard", "Berry")
